300568893603ab100c34668d8488e14cf64cc950
Riya <riyaraj0409@gmail.com>
2022-04-09 01:38:11 +0530

    Lesson 11 - Text Connection

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,20 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataCollection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
+
+
         // TODO - Wire up the CreatePrize for the text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
             // Load the text file
             // Convert the text to list<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModel();
+
             // Find the max id
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
             // Add the new record with the new id (max + 1)
+            prizes.Add(model);
+            
             // Convert the prizes to a list of string
             // Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
         }
     }
 }
