6dda7eff87f9586e476a9d89d82bcfc45f3b3ebe
Riya <riyaraj0409@gmail.com>
2022-04-25 16:49:07 +0530

    Create Tournament - Part 1

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -85,5 +85,10 @@ namespace TrackerLibrary.DataAccess
         {
             return PeopleFile.FullFilePath().LoadFile().ConvertToPersonModel();
         }
+
+        public List<TeamModel> GetTeam_All()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
     public partial class CreateTournamentForm : Form
     {
+        List<TeamModel> availableTeams = GlobalConfig.Connection.GetTeam_All();
+        List<TeamModel> selectedTeams = new List<TeamModel>();
+        List<PrizeModel> selectedPrizes = new List<PrizeModel>();
+
         public CreateTournamentForm()
         {
             InitializeComponent();
+            WiredUpLists();
+        }
+
+        private void WiredUpLists()
+        {
+            selectTeamDropDown.DataSource = null;
+            selectTeamDropDown.DataSource = availableTeams;
+            selectTeamDropDown.DisplayMember = "TeamName";
+
+            tournamentTeamsListBox.DataSource = null;
+            tournamentTeamsListBox.DataSource = selectedTeams;
+            tournamentTeamsListBox.DisplayMember = "TeamName";
+
+            prizeListBox.DataSource = null;
+            prizeListBox.DataSource = selectedPrizes;
+            prizeListBox.DisplayMember = "PlaceName";
         }
 
         private void teamOneScoreValue_TextChanged(object sender, EventArgs e)
@@ -24,7 +46,15 @@ namespace TrackerUI
 
         private void addTeamButton_Click(object sender, EventArgs e)
         {
+            TeamModel t = (TeamModel)selectTeamDropDown.SelectedItem;
 
+            if (t != null)
+            {
+                availableTeams.Remove(t);
+                selectedTeams.Add(t);
+
+                WiredUpLists();
+            }
         }
     }
 }
